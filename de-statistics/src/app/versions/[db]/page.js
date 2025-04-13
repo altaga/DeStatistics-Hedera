36f2e25004d11fcac45013c74525f540f8bb4d4d
Víctor Altamirano <v.a.i@hotmail.com>
2025-04-13 17:14:22 -0600

    commit

--- de-statistics/src/app/versions/[db]/page.js
+++ de-statistics/src/app/versions/[db]/page.js
@@ -1,31 +1,19 @@
-import { getDB } from "@/actions/recallServer";
 import SimpleCharts from "@/app/versions/[db]/components/chart";
 import styles from "@/app/versions/[db]/page.module.css";
 import ReactDiff from "./components/reactDiff";
+import { getAllFetch, getDB } from "@/actions/hederaServer";
 
-/*
-// Example Ethereum address
-const ethAddress = "0x4bb24Db28959248E00792aDAF04a8EB32C5AB3Ef";
-
-// Derive the Filecoin address (f4 address) from the Ethereum address
-const filecoinAddress = newDelegatedEthAddress(ethAddress);
-
-console.log(`Ethereum Address: ${ethAddress}`);
-console.log(`Filecoin Address: ${filecoinAddress.toString()}`);
-*/
-
-export const revalidate = 60; // Check every 60 seconds for updates
+export const revalidate = parseInt(process.env.REVALIDATE); // Check every 60 seconds for updates
 export const dynamicParams = true; // Dynamically generate params
 
 export async function generateStaticParams() {
-  const res = await fetch(process.env.RECALL_URL);
-  const posts = await res.json();
-  return posts.data.map((post) => ({
+  const posts = await getAllFetch();
+  return posts.map((post) => ({
     db: post.key,
   }));
 }
 
-export default async function Statistic({ params }) {
+export default async function Versions({ params }) {
   // Get Statistic Server Side
   const { db } = await params;
   const data = await getDB(db);
